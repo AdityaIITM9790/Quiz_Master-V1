bbac09937bbc0c7f3c4863ede4ca6b146a339e4e
AdityaIITM9790 <21f1004669@ds.study.iitm.ac.in>
2025-03-20 01:46:34 +0530

    The main application file which will host and run application

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_login import LoginManager
+from applications.database import db#3 database
+
+app = None
+
+def create_app():
+    app = Flask(__name__)
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quiz_master.db'
+    db.init_app(app)
+
+    # Push app context at runtime
+    app.app_context().push()
+
+    return app
+
+# Create the app instance
+app = create_app()
+from applications.controllers import *
+from applications.models import *
+# from applications.resources import *
+
+if __name__ == '__main__':
+    app.run(debug=True)
